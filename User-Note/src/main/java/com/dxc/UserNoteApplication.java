0a7e25ac58675bfa9157382a552adc1dad7db67b
Lakshmi25 <l.kothapalli@dxc.com>
2023-07-03 14:22:58 +0530

    keepNotes

---
+++ User-Note/src/main/java/com/dxc/UserNoteApplication.java
@@ -0,0 +1,15 @@
+package com.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserNoteApplication.class, args);
+		System.out.println("KeepNotes Application Started");
+	}
+	
+
+}
---
+++ User-Note/src/main/java/com/dxc/repo/UserRepository.java
@@ -0,0 +1,15 @@
+package com.dxc.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dxc.model.User;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, String>{
+
+	Optional<User> findByUserIdAndUserPassword(String userId, String userPassword);
+
+}
